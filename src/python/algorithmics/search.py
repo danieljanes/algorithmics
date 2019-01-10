c5c6ef1c846d0f5d204629cdbc28499e73199482
Daniel <dnl.btl@gmail.com>
2019-01-10 21:55:31 +0100

    Add type hints to search module

--- src/python/algorithmics/search.py
+++ src/python/algorithmics/search.py
@@ -1,5 +1,9 @@
-def linear_search(list, key):
-    i = 0
+from typing import Any
+from typing import List
+
+
+def linear_search(list: List[Any], key: Any) -> bool:
+    i: int = 0
     while i < len(list):
         if list[i] == key:
             return True
@@ -7,11 +11,11 @@ def linear_search(list, key):
     return False
 
 
-def binary_search(list, key):
-    l = 0
-    r = len(list) - 1
+def binary_search(list: List[Any], key: Any) -> bool:
+    l: int = 0
+    r: int = len(list) - 1
     while l < r:
-        i = l + (r-l) // 2
+        i: int = l + (r-l) // 2
         item = list[i]
         if item == key:
             return True
